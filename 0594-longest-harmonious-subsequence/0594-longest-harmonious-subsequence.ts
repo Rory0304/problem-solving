1d710f438b36f9421f1e3015fa34689b0f614108
Rory <42709747+Rory0304@users.noreply.github.com>
2025-07-13 19:48:56 +0900

    Time: 12 ms (95.71%), Space: 59.7 MB (98.08%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.ts
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.ts
@@ -1,19 +1,27 @@
 function findLHS(nums: number[]): number {
-    // 꼭 연속적일 필요는 없음
-    const hashMap = new Map();
-
-    for(let num of nums){
-        hashMap.set(num, (hashMap.get(num) || 0) + 1);
+    // 풀이 방법
+    // 1. HashMap
+    // 2. Windowing
+    if(nums.length < 2){
+        return 0
     }
 
-    const keys = hashMap.keys();
-    let maxLength = 0
+    // 단순 sort 는 문자열 기준이기 때문에 수치 정렬로 변환한다.
+    const sortedNums = new Int32Array(nums);
+    sortedNums.sort();
 
-    for(let key of keys){
-        if(hashMap.get(key+1)){
-            maxLength = Math.max(hashMap.get(key) + hashMap.get(key+1), maxLength)
+    let left = 0;
+    let maxLength = 0;
+    
+    for(let right=0;right<sortedNums.length;right++){       
+        while (sortedNums[right] - sortedNums[left] > 1) {
+            left++
+        } 
+
+        if(sortedNums[right] - sortedNums[left] === 1){
+            maxLength = Math.max(maxLength, right-left+1)
         }
     }
-
+    
     return maxLength
 };
