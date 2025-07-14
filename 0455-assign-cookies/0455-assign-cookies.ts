cf5c7bd859ec24611803ab34efcb37ec3f4718c5
Rory <42709747+Rory0304@users.noreply.github.com>
2025-07-14 17:05:05 +0900

    Time: 26 ms (63.6%), Space: 62.6 MB (81.99%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.ts
+++ 0455-assign-cookies/0455-assign-cookies.ts
@@ -3,13 +3,17 @@ function findContentChildren(g: number[], s: number[]): number {
     // cookie j
     // s[j] (j의 size) >= g[i] => j를 i에 할당 가능
 
-    let count = 0
     g.sort((a, b) => a-b);
     s.sort((a, b) => a-b);
 
-    for(const cookie of s){
-        if(cookie >= g[count]) count++
-        // 이보다 큰 지수의 child 에게 제공하지 못하면, 해당 count 를 반환
+    let i=0, j=0, count=0;
+
+    while(i<g.length && j<s.length){
+        if(s[j] >= g[i]){
+            count++;
+            i++;
+        }
+        j++
     }
 
     return count
