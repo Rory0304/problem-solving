d76e16573ee0fe7254e1bf6f461c13bffb38ffaf
Rory <42709747+Rory0304@users.noreply.github.com>
2025-07-20 21:17:55 +0900

    Time: 53 ms (85.32%), Space: 129.6 MB (36.57%) - LeetHub

--- LeetCode/Medium/0077-combinations/0077-combinations.ts
+++ LeetCode/Medium/0077-combinations/0077-combinations.ts
@@ -1,25 +1,25 @@
 function combine(n: number, k: number): number[][] {
-  // 1~n까지 k 를 뽑을 수 있는 모든 경우의 수 구하기
-  let answer: number[][] = [];
-  let temp = []
-  
-  function dfs(current: number) {
-    if(temp.length === k){
-        answer.push(temp.slice());
-        return;
+    let answer: number[][] = []
+    let temp = []
+
+    function dfs(current){
+        if(temp.length === k){
+            answer.push(temp.slice())
+            return;
+        }
+
+        if(current > n){
+            return;
+        }
+        
+        for(let i=current;i<=n;i++){
+            temp.push(i)
+            dfs(i+1);
+            temp.pop()
+        }
     }
 
-    if(current > n){
-        return;
-    }
+    dfs(1)
 
-    temp.push(current)
-    dfs(current+1)
-    temp.pop();
-    dfs(current+1)
-  } 
-
-  dfs(1) 
-
-  return answer
+    return answer    
 };
