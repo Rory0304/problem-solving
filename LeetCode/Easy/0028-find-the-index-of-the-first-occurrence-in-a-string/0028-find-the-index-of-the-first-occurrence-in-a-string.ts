b6d3f8426b83d91e564ece304b90fddeb696d46d
Rory <42709747+Rory0304@users.noreply.github.com>
2025-09-19 19:47:30 +0900

    Time: 2 ms (14.88%), Space: 54.5 MB (84.98%) - LeetHub

--- LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
+++ LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -2,8 +2,19 @@ function strStr(haystack: string, needle: string): number {
     // check is first letter matched with needle
     for(let i=0;i<haystack.length;i++){
         if(haystack[i] === needle[0]){
-            let window = haystack.substring(i, i + needle.length)
-            if(window === needle){
+            let start = i
+            let count = 1
+            // check for loop
+            while(count < needle.length){
+                if(haystack[i+count] === needle[count]){
+                    count++
+                }
+                else{
+                    break;
+                }
+            }
+
+            if(count === needle.length){
                 return i
             }
         }
