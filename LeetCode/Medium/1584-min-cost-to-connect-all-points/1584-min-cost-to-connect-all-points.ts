f2d20ef547633d357f1adcaaba888bf6f9131a16
Rory <42709747+Rory0304@users.noreply.github.com>
2025-09-29 20:15:27 +0900

    Time: 2089 ms (20.27%), Space: 113.3 MB (56.76%) - LeetHub

--- LeetCode/Medium/1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.ts
+++ LeetCode/Medium/1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.ts
@@ -1,46 +1,43 @@
 function minCostConnectPoints(points: number[][]): number {
-    // 각 노드를 연결했을 때의 비용을 계산
     const costs = [];
-    let answer = 0;
+    let answer = 0
 
+    // 간선들의 가중치를 먼저 구한다
     for (let i = 0; i < points.length; i++) {
         for (let j = i + 1; j < points.length; j++) {
             const [x1, y1] = points[i]
             const [x2, y2] = points[j]
 
             const distance = Math.abs(x1 - x2) + Math.abs(y1 - y2)
-            costs.push([i, j, distance]);
+            costs.push([i, j, distance])
         }
     }
 
-    // 정렬
+    // 가중치를 기준으로 정렬한다.
     costs.sort((a, b) => a[2] - b[2])
 
-    // parent 초기화
+    // 각 정점의 parent 초기화
     const parent = new Map();
-    for(let i=0;i<points.length;i++){
+    for (let i = 0; i < points.length; i++) {
         parent.set(i, i)
     }
 
-    const getParent = (parent, n1) => {
-        if (parent.get(n1) === n1) return n1
+    function getParent(parent, n) {
+        if (parent.get(n) === n) return n;
 
-        return parent[n1] = getParent(parent, parent.get(n1))
+        return getParent(parent, parent.get(n))
     }
 
-    const sameParent = (parent, n1, n2) => {
-        return getParent(parent, n1) === getParent(parent, n2)
-    }
+    function union(parent, a, b) {
+        const aParent = getParent(parent, a)
+        const bParent = getParent(parent, b)
 
-    const union = (parent, n1, n2) => {
-        const n1Parent = getParent(parent, n1)
-        const n2Parent = getParent(parent, n2)
-
-        if(n1Parent === n2Parent){
+        if (aParent === bParent) {
             return false
         }
 
-        parent.set(n2Parent, n1Parent)
+        parent.set(bParent, aParent)
+
         return true
     }
 
@@ -48,6 +45,7 @@ function minCostConnectPoints(points: number[][]): number {
     costs.forEach((cost) => {
         const [start, end, dist] = cost
 
+        // 
         if (union(parent, start, end)) {
             answer += dist;
         }
