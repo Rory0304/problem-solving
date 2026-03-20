c98f3a00a95bbcb7af71cc035e3c2ec2da23a387
Rory <42709747+Rory0304@users.noreply.github.com>
2026-03-20 17:35:17 +0900

    Time: 24 ms (46.89%), Space: 66.3 MB (36.51%) - LeetHub

--- LeetCode/Medium/0707-design-linked-list/0707-design-linked-list.ts
+++ LeetCode/Medium/0707-design-linked-list/0707-design-linked-list.ts
@@ -1,87 +1,74 @@
-interface Node {
-    val: number
-    next: Node
+type Node = {
+    val: number;
+    next: Node;
 }
 
-
-// 
 class MyLinkedList {
     head: Node;
     size: number;
 
     constructor() {
-        this.head = null
-        this.size = 0
-    }
-
-    getNode(index: number): Node | null {
-        let node = this.head;
-        let count = 0;
-
-        while (count < index) {
-            node = node.next
-            count++
-        }
-
-        return node
+        this.head = null;
+        this.size = 0;
     }
 
     get(index: number): number {
-        const node = this.getNode(index);
-        if (!node) return -1
-        return node.val
+        if (index < 0 || index >= this.size) return -1;
+
+        let current = this.head;
+
+        for (let i = 0; i < index; i += 1) {
+            current = current.next;
+        }
+
+        return current.val;
     }
 
     addAtHead(val: number): void {
-        const next = this.head
-        this.head = { val, next }
-        this.size++
+        this.addAtIndex(0, val);
     }
 
     addAtTail(val: number): void {
-        if(this.size === 0){
-            this.addAtHead(val)
-            return 
-        }
-        const node = this.getNode(this.size - 1)
-        node.next = { val, next: null }
-        this.size++
+        this.addAtIndex(this.size, val)
     }
 
     addAtIndex(index: number, val: number): void {
+        // 예외 케이스
+        if (index < 0 || index > this.size) return;
+
+        this.size++;
+
         if (index === 0) {
-            this.addAtHead(val)
+            const newHead = { val, next: this.head };
+            this.head = newHead;
             return;
         }
 
-        if (index === this.size) {
-            this.addAtTail(val)
-            return
+        let current = this.head;
+        for (let i = 0; i < index - 1; i++) {
+            current = current?.next;
         }
 
-        const node = this.getNode(index - 1)
-        node.next = { val, next: node.next }
-        this.size++
+        current.next = { val, next: current.next }
     }
 
     deleteAtIndex(index: number): void {
-        if(index >= this.size){
+        // 예외 케이스
+        if (index < 0 || index >= this.size) return;
+
+        this.size--;
+
+        if (index === 0) {
+            this.head = this.head?.next;
             return;
         }
 
-        if(this.size === 0){
-            return
+        let current = this.head;
+        for (let i = 0; i < index - 1; i++) {
+            current = current?.next;
         }
 
-        if (index === 0) {
-            this.head = this.head.next;
-            this.size--
-            return
-        }
-
-        const prevNode = this.getNode(index - 1);
-        prevNode.next = prevNode.next?.next || null
-        this.size--
+        current.next = current.next?.next;
     }
 }
 
